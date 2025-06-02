295d813a71c55b3e6e00036cab91751c5a9ca3e8
PlayPayBD <shahriaraume241@gmail.com>
2025-06-02 22:19:45 +0600

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👋 Welcome to PlayPay</h1>
+      <p>Made by PlaypayBD & ChatGPT</p>
+      <a href="/game" style={{ color: "blue", fontSize: "20px" }}>
+        Go to Game 🎮
+      </a>
+    </div>
+  );
+}
